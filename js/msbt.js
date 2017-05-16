f26c5350bc39ff48d0f712b59eb552430617f43b
AK97 <ashwinkumar97@hotmail.com>
2017-05-16 18:52:58 -0400

    tweaked lab nb,prepping for full implem; person.html

--- js/msbt.js
+++ js/msbt.js
@@ -5,5 +5,25 @@ function loadBody() {
 	var h = $(window).height() - $('#nav').height(); 
 	h -= 20;
 	$('#pageContainer').css('height', h + 'px');
-	$('#sidebar').css('height', h + 'px');
+	//$('#sidebar').css('height', h + 'px');
 }
+$(window).resize(function() {
+	loadBody();
+});
+function ln(a) {
+	//$("#pdf").attr('src', a + '.pdf');
+	var parent = $('#pdf').parent();
+	var newElement = "<embed src='" + a + ".pdf'" + " id='pdf' width='100%' height='100%' type='application/pdf'>";
+	$('#pdf').remove();
+	parent.append(newElement);
+}
+/*
+//FOR WHEN ALL LAB NOTEBOOK ENTRIES ARE COMPLETE, PARSED, & CONVERTED TO PDF//
+function loadEntries() {
+	var num_entries = 57
+	for(i = 1; i < num_entries; i++) {
+		var elem = "<li onClick='ln(" + i + ")'><a class='subtitle'>Entry " + i + "</a></li>";
+		$('#sidebar').append(elem);
+	}
+}
+*/
