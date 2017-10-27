f750bd520d4f8f9ea62d4c7c447d1eb9d09b1e32
AK97 <ashwinku@umich.edu>
2017-10-26 22:46:37 -0400

    improved framework, added more necessary pages. todo:change Sumana; add content

---
+++ js/team.js
@@ -0,0 +1,252 @@
+var team = [
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+    {
+        "name": "Marcus Ammerlaan",
+        "role": "Faculty Advisor",
+        "bio": "",
+        "pic": "images/team/marcus.png"
+    },
+];
+
+window.onload = function() {
+	document.getElementById("words").innerHTML = team[0].game;
+};
